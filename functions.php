af79b84e711528375ef42497fd04f4cdaa2b22be
Alex Leuschner <alex.leuschner@gmail.com>
2022-06-27 16:39:26 -0400

    modify default wordpress page

--- functions.php
+++ functions.php
@@ -22,6 +22,26 @@ function hello_elementor_child_enqueue_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts' );
 
+function my_custom_logo() { ?>
+     <style type="text/css">
+     	body.login {
+			
+		}
+         #login h1 a, .login h1 a {
+            background-image: none;
+            content: 'Login to Beams by Design';
+			color: #000;
+			font-family: 'Roboto', sams-serif;
+			text-indent: 0;
+			width: 300px;
+			height: auto;
+			text-transform: uppercase;
+			font-weight: 300;
+         }
+     </style>
+ <?php } 
+add_action( 'login_enqueue_scripts', 'my_custom_logo' );
+
 // Hide Admin from Customers
 add_action('after_setup_theme', 'remove_admin_bar');
 function remove_admin_bar() {
@@ -30,7 +50,15 @@ function remove_admin_bar() {
 	}
 }
 
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
 
+function my_login_logo_url_title() {
+    return 'Beams by Design';
+}
+add_filter( 'login_headertext', 'my_login_logo_url_title' );
 
 include_once('inc/cpt-proposal.php');
 include_once('inc/shortcodes-proposals.php');
